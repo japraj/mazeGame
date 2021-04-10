cb064bd15870293e4daf884897ec97f77ea772ca
japraj <japraj@live.ca>
2021-04-10 00:46:31 -0700

    remove unused exception

--- src/main/model/exceptions/VisitedNodeException.java
+++
@@ -1,6 +0,0 @@
-package model.exceptions;
-
-// An exception thrown when a node passed as an argument (implicit or explicit) has already been visited
-public class VisitedNodeException extends Exception {
-
-}
