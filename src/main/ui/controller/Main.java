5719faf0800ccb5a82eb0cf2141102e3779f4fb6
japraj <japraj@live.ca>
2021-04-10 00:26:23 -0700

    add look & feel lib

--- src/main/ui/controller/Main.java
+++ src/main/ui/controller/Main.java
@@ -1,9 +1,18 @@
 package ui.controller;
 
-// Initialize the application
+import com.formdev.flatlaf.FlatIntelliJLaf;
+
+import javax.swing.*;
+
+// Set look & feel and initialize the application
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        try {
+            UIManager.setLookAndFeel(new FlatIntelliJLaf());
+        } catch (Exception e) {
+            System.err.println("Failed to initialize Look & Feel");
+        }
         new MazeGame();
     }
 
